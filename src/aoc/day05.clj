3bc89e03e639e00e17eee9bd5866d91aec43d066
Alvydas Vitkauskas <alvydas@ondato.com>
2023-08-11 00:42:05 +0300

    day 05

--- src/aoc/day05.clj
+++ src/aoc/day05.clj
@@ -25,6 +25,16 @@
        (drop-while (fn [[_ {:keys [pos table]}]] (some? (get table pos))))
        ffirst))
 
+(defn part1-v3 []
+  (loop [table input
+         pos 0
+         jumps 0]
+    (if-let [offset (get table pos)]
+      (let [new-table (assoc table pos (inc offset))
+            new-pos (+ pos offset)]
+        (recur new-table new-pos (inc jumps)))
+      jumps)))
+
 (defn jump-part2 [{:keys [pos table]}]
   (let [offset (get table pos)
         new-pos (+ pos offset)
@@ -35,10 +45,25 @@
 (defn part2 []
   (reduce check-state 0 (iterate jump-part2 {:pos 0 :table input})))
 
+(defn part2-v2 []
+  (loop [table input
+         pos 0
+         jumps 0]
+    (if-let [offset (get table pos)]
+      (let [new-offset (if (> offset 2) (dec offset) (inc offset))
+            new-table (assoc table pos new-offset)
+            new-pos (+ pos offset)]
+        (recur new-table new-pos (inc jumps)))
+      jumps)))
+
 (comment
   (part1)
   (part1-v2)
+  (part1-v3)
   (dotimes [_ 10] (time (dotimes [_ 10] (part1)))) ; "Elapsed time: 480.911584 msecs"
   (dotimes [_ 10] (time (dotimes [_ 10] (part1-v2)))) ; "Elapsed time: 738.415167 msecs"
+  (dotimes [_ 10] (time (dotimes [_ 10] (part1-v3)))) ; "Elapsed time: 306.228375 msecs"
   (part2)
-  (dotimes [_ 10] (time (part2)))) ; "Elapsed time: 3388.156667 msecs"
+  (part2-v2)
+  (dotimes [_ 10] (time (part2))) ; "Elapsed time: 3388.156667 msecs"
+  (dotimes [_ 10] (time (part2-v2)))) ; "Elapsed time: 2084.809667 msecs"
